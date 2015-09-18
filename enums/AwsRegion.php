e6a100bfc896f989c26a423ff9ce838e8d205bb5
programster <sdpagent@gmail.com>
2015-09-18 11:00:08 +0000

    converting tabs to spaces in awsregion

--- enums/AwsRegion.php
+++ enums/AwsRegion.php
@@ -18,52 +18,52 @@ class AwsRegion
         return $region;
     }
     
-	public static function create_virginia()
+    public static function create_virginia()
     {
         return self::create_US_E1();
     }
     
-	public static function create_US_W1()
+    public static function create_US_W1()
     {
         $region = new AwsRegion('us-west-1');
         return $region;
     }
     
-	public static function create_california()
+    public static function create_california()
     {
         return self::create_US_W1();
     }
     
-	public static function create_US_W2()
+    public static function create_US_W2()
     {
         $region = new AwsRegion('us-west-2');
         return $region;
     }
     
-	public static function create_oregon()
+    public static function create_oregon()
     {
         return self::create_US_W2();
     }
     
-	public static function create_EU_W1()
+    public static function create_EU_W1()
     {
         $region = new AwsRegion('eu-west-1');
         return $region;
     }
     
-	public static function create_ireland()
+    public static function create_ireland()
     {
         return self::create_EU_W1();
         return $region;
     }
     
-	public static function create_APAC_SE1()
+    public static function create_APAC_SE1()
     {
         $region = new AwsRegion('ap-southeast-1');
         return $region;
     }
     
-	public static function create_singapore()
+    public static function create_singapore()
     {
         return self::create_APAC_SE1();
     }
@@ -75,34 +75,34 @@ class AwsRegion
     }
     
     public static function create_sydney()
-    {        
+    {
         return self::create_APAC_SE2();
     }
     
-	public static function create_APAC_NE1()
+    public static function create_APAC_NE1()
     {
         $region = new AwsRegion('ap-northeast-1');
         return $region;
     }
     
-	public static function create_tokyo()
+    public static function create_tokyo()
     {
         return self::create_APAC_NE1();
     }
     
-	public static function create_US_GOV1()
+    public static function create_US_GOV1()
     {
         $region = new AwsRegion('us-gov-west-1');
         return $region;
     }
     
-	public static function create_SA_E1()
+    public static function create_SA_E1()
     {
         $region = new AwsRegion('sa-east-1');
         return $region;
     }
     
-	public static function create_sao_paulo()
+    public static function create_sao_paulo()
     {
         return self::create_SA_E1();
     }
